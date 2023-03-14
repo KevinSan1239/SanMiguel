bb98e3197d9d3891e50061305031776601485128
KSanchez <kssanchezc@itc.edu.co>
2023-03-14 13:50:40 -0500

    Revert "se subio el proyecto"
    This reverts commit 3c6ed5736a25d6654185bf7e804dcafcd5bf2f99.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ClientesComponent } from './Components/clientes/clientes.component';
-import { DevolucionesComponent } from './Components/devoluciones/devoluciones.component';
-import { FacturasComponent } from './Components/facturas/facturas.component';
-import { FormComponent } from './Components/form/form.component';
-import { InventariosComponent } from './Components/inventarios/inventarios.component';
-import { PersonasComponent } from './Components/personas/personas.component';
-import { ProductosComponent } from './Components/productos/productos.component';
-import { TablaComponent } from './Components/tabla/tabla.component';
-import { UsuariosComponent } from './Components/usuarios/usuarios.component';
 
-const routes: Routes = [
-
-    {path:"facturas",component:FacturasComponent},
-    {path:"clientes",component:ClientesComponent},
-    {path:"usuarios",component:UsuariosComponent},
-    {path:"inventarios",component:InventariosComponent},
-    {path:"productos",component:ProductosComponent},
-    {path:"devoluciones",component:DevolucionesComponent},
-    {path:"tabla",component:TablaComponent},
-    {path:"formulario",component:FormComponent},
-    {path:"personas",component:PersonasComponent}
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
